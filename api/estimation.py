49e7f0405c4ba28fee8b23ca68073c9d3b748004
Tanmay Munjal <tanmaymunjal64@gmail.com>
2023-10-15 19:34:47 -0600

    decreased hourly rate

--- api/estimation.py
+++ api/estimation.py
@@ -3,4 +3,4 @@ from flask import make_response
 
 # keeping it sample for now
 def estimate_time():
-    return make_response({"time": 3 * 30 * 24 * 60, "cost": 86400}, 200)
+    return make_response({"time": 3 * 30 * 24 * 60, "cost": 360}, 200)
